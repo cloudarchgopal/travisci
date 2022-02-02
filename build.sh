423bfc165e70ee3656d7b88fe94c112bb0accdda
gopal das <gopal1409@gmail.com>
2022-02-02 16:30:19 +0530

    updated with build

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# sudo podman build -t hello-world .
+docker build -t hello-world
+
---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# sudo podman run -it -p 8080:8080 hello-world
+docker run -it -p 8080:8080 hello-world
+
